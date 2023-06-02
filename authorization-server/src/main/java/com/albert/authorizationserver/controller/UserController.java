0259908315523654ecba30051b3db2f42ef424d0
ALbertIM0427 <guang7226961@naver.com>
2023-06-02 15:36:08 +0900

    feat: ErrorSerializer 구현

--- authorization-server/src/main/java/com/albert/authorizationserver/controller/UserController.java
+++ authorization-server/src/main/java/com/albert/authorizationserver/controller/UserController.java
@@ -5,10 +5,11 @@ import com.albert.authorizationserver.dto.JoinRequest;
 import com.albert.authorizationserver.model.User;
 import com.albert.authorizationserver.service.UserService;
 import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,9 +19,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity addUser(@Valid JoinRequest joinRequest, BindingResult bindingResult) {
+    public ResponseEntity addUser(@Valid JoinRequest joinRequest, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(errors);
+        }
         User user = userService.save(joinRequest);
         userService.login(user);
-        return ResponseEntity.ok().body(user.getEmail() + "가입 성공했습니다.");
+        return ResponseEntity.created(URI.create("/login")).body("성공적으로 유저를 추가했습니다.");
     }
 }
