5edd00bf6bf30218e30d3dac239503093e614b10
ALbertIM0427 <guang7226961@naver.com>
2023-06-08 21:45:15 +0900

    refactor: csrf disable 구현

--- api/src/main/java/com/albert/commerce/config/security/SecurityConfig.java
+++ api/src/main/java/com/albert/commerce/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
@@ -25,6 +26,7 @@ public class SecurityConfig {
                         .requestMatchers("/index").permitAll()
                         .anyRequest().authenticated()
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .oauth2Login(oAuth2LoginConfigurer -> oAuth2LoginConfigurer.successHandler(
                         customAuthenticationSuccessHandler));
         return httpSecurity.build();
