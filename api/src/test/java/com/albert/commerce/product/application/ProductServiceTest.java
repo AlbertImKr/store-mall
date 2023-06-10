8c689aef74eb1c07f211b609f7a1c5f58b1e0b42
ALbertIM0427 <guang7226961@naver.com>
2023-06-10 21:40:28 +0900

    docs: api 문서 수정

--- api/src/test/java/com/albert/commerce/product/application/ProductServiceTest.java
+++ api/src/test/java/com/albert/commerce/product/application/ProductServiceTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import com.albert.commerce.product.command.domain.Product;
-import com.albert.commerce.product.command.domain.ProductId;
 import com.albert.commerce.product.command.domain.ProductRepository;
 import com.albert.commerce.store.command.domain.StoreId;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,19 +38,5 @@ class ProductServiceTest {
 
         // then
         verify(productRepository).save(any(Product.class));
-    }
-
-    @DisplayName("product를 제거한다")
-    @Test
-    void removeProduct() {
-        // given
-        ProductId productId = new ProductId();
-        given(productRepository.deleteProductByProductId(any(ProductId.class))).willReturn(true);
-
-        // when
-        productService.removeProduct(productId);
-
-        // then
-        verify(productRepository).deleteProductByProductId(any(ProductId.class));
     }
 }
