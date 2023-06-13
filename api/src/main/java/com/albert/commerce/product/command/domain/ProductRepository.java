189c185851481c725d5787510fb9a8a873cab5d0
ALbertIM0427 <guang7226961@naver.com>
2023-06-13 17:22:20 +0900

    refactor: 필요 없는 메서드 삭제

--- api/src/main/java/com/albert/commerce/product/command/domain/ProductRepository.java
+++ api/src/main/java/com/albert/commerce/product/command/domain/ProductRepository.java
@@ -3,6 +3,4 @@ package com.albert.commerce.product.command.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, ProductId> {
-
-    boolean deleteProductByProductId(ProductId productId);
 }
