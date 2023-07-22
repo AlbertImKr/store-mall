4167b1544ea96533a699bfb980559086bd57971f
ALbertIM0427 <guang7226961@naver.com>
2023-07-22 15:52:32 +0900

    refactor: Money record value() 제거

--- api/src/main/java/com/albert/commerce/common/infra/persistence/Money.java
+++ api/src/main/java/com/albert/commerce/common/infra/persistence/Money.java
@@ -2,14 +2,7 @@ package com.albert.commerce.common.infra.persistence;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
-public record Money(long value) {
-
-    @Override
-    @JsonValue
-    public long value() {
-        return value;
-    }
-
+public record Money(@JsonValue long value) {
 
     public static Money from(long value) {
         return new Money(value);
