441f451c22456a87028a7e3c54ef578ff446e856
ALbertIM0427 <guang7226961@naver.com>
2023-08-09 18:30:54 +0900

    feat: DomainEventMessageConfig 구현

---
+++ shared/src/main/java/com/albert/shared/config/messaging/domainevent/DomainEventMessageConfig.java
@@ -0,0 +1,112 @@
+package com.albert.shared.config.messaging.domainevent;
+
+import com.albert.shared.messaging.domain.event.DomainEvent;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.integration.endpoint.EventDrivenConsumer;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class DomainEventMessageConfig {
+
+    @Value("${commerce.messaging.base-package.domain-event-listener}")
+    private String domainEventListenerBasePackage;
+    @Value("${commerce.messaging.base-package.domain-event}")
+    private String domainEventBasePackage;
+
+    @Bean
+    public Set<String> domainEventClassNames() {
+        Reflections reflections = new Reflections(
+                new ConfigurationBuilder()
+                        .setUrls(ClasspathHelper.forPackage(domainEventListenerBasePackage))
+                        .setScanners(new MethodAnnotationsScanner()));
+        Set<Method> methods = reflections.getMethodsAnnotatedWith(ServiceActivator.class);
+
+        return methods.stream()
+                .filter(method -> method.getParameterTypes().length != 0)
+                .filter(method -> DomainEvent.class.isAssignableFrom(method.getParameterTypes()[0]))
+                .map(method -> method.getParameterTypes()[0].getName())
+                .collect(Collectors.toSet());
+    }
+
+    @Bean
+    public DomainEventClassResolver domainEventClassResolver() {
+        Reflections reflections = new Reflections(domainEventBasePackage);
+        Set<Class<? extends DomainEvent>> domainEventClasses = reflections.getSubTypesOf(DomainEvent.class);
+        return new DomainEventClassResolver(domainEventClasses);
+    }
+
+    @Bean
+    public PublishSubscribeChannel domainEventChannel(ThreadPoolTaskExecutor messageTaskExecutor) {
+        return new PublishSubscribeChannel(messageTaskExecutor);
+    }
+
+    @Bean
+    public EventDrivenConsumer domainEventHandler(PublishSubscribeChannel domainEventChannel,
+            MessageHandler domainEventSender) {
+        EventDrivenConsumer consumer = new EventDrivenConsumer(domainEventChannel, domainEventSender);
+        consumer.start();
+        return consumer;
+    }
+
+    @Bean
+    public MessageHandler domainEventSender(Set<String> domainEventClassNames) {
+        return message -> {
+            String domainEventClassName = message.getPayload().getClass().getName();
+            if (domainEventClassNames.contains(domainEventClassName)) {
+                Optional<PublishSubscribeChannel> channel = DomainEventChannelRegistry.findChannel(
+                        domainEventClassName);
+                if (channel.isPresent()) {
+                    channel.get().send(message);
+                } else {
+                    throw new IllegalArgumentException(
+                            "No channel found for domain event class: " + domainEventClassName);
+                }
+            } else {
+                throw new IllegalArgumentException("Unsupported domain event class: " + domainEventClassName);
+            }
+        };
+    }
+
+
+    @Component
+    private static class DomainEventChannelRegistry {
+
+        private static final Map<String, PublishSubscribeChannel> channels = new ConcurrentHashMap<>();
+
+        public DomainEventChannelRegistry(Set<String> domainEventClassNames, Executor messageTaskExecutor,
+                ConfigurableListableBeanFactory beanFactory) {
+            for (String className : domainEventClassNames) {
+                PublishSubscribeChannel channel = new PublishSubscribeChannel(messageTaskExecutor, true);
+                beanFactory.registerSingleton(className, channel);
+                this.registerChannel(className, channel);
+            }
+        }
+
+        public static Optional<PublishSubscribeChannel> findChannel(String className) {
+            return Optional.ofNullable(channels.get(className));
+        }
+
+        public void registerChannel(String className, PublishSubscribeChannel channel) {
+            channels.put(className, channel);
+        }
+    }
+
+}
