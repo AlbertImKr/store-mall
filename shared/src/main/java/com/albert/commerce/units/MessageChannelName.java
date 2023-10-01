692daa21c32551e230cb29ed5e5d1db4add36483
ALbertIM0427 <guang7226961@naver.com>
2023-10-01 13:53:27 +0900

    fix: 잘못 된 channel 이름 수정

--- shared/src/main/java/com/albert/commerce/units/MessageChannelName.java
+++ shared/src/main/java/com/albert/commerce/units/MessageChannelName.java
@@ -2,7 +2,7 @@ package com.albert.commerce.units;
 
 public class MessageChannelName {
 
-    public static final String USER_UPLOAD_CHANNEL = "user-upload-channel";
+    public static final String USER_UPLOAD_CHANNEL = "UserUploadCommand";
 
     private MessageChannelName() {
         throw new IllegalStateException("Utility class");
